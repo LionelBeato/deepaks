493f85e14cfc8d8a14d8cb43fc9336ffd290a8a0
Leo <lionel.beato@gmail.com>
2019-06-20 21:59:36 -0400

    okay, I'm starting this thing up again

--- src/deepaks/FinanceConsole.java
+++ src/deepaks/FinanceConsole.java
@@ -1,5 +1,6 @@
 package deepaks;
 
 public class FinanceConsole {
+	
 
 }
--- src/deepaks/Inventory.java
+++ src/deepaks/Inventory.java
@@ -1,13 +1,27 @@
 package deepaks;
 
+import java.util.*;
+
 public class Inventory {
 	
 	//TODO Create a hash map of the inventory. The key will be the SKU number.
 	
+	public static HashMap<Integer, String> inventory() {
+		
+		HashMap<Integer, String> inventHash = new HashMap<>();
+		inventHash.put(3432, "Car 1");
+		inventHash.put(1232, "Car 2");
+		inventHash.put(4562, "Car 3");
+		
+		return inventHash;
+		
+		
+	}
 	//TODO create an arraylist for the car values
 	
 	
+	public static void main (String[] args) {
+		System.out.println(inventory());
+	}
 	
-	
-
 }
